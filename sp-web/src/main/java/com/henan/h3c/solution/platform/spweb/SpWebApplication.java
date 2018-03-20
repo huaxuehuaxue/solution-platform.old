dd5cf8dab2b9c2274ee7f834feca0c09f159647f
chenyuqg <chenyuqg@hotmail.com>
2018-03-20 13:33:35 +0800

    original commit.

---
+++ sp-web/src/main/java/com/henan/h3c/solution/platform/spweb/SpWebApplication.java
@@ -0,0 +1,12 @@
+package com.henan.h3c.solution.platform.spweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpWebApplication.class, args);
+	}
+}
